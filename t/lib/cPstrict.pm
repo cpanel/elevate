24f3a69041f9887f8d4035836b5ed44a47809008
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2022-01-25 12:40:52 -0600

    Update copyrights

--- t/lib/cPstrict.pm
+++ t/lib/cPstrict.pm
@@ -1,7 +1,7 @@
 package cPstrict;
 
-# cpanel - cPstrict.pm                             Copyright 2022 cPanel, L.L.C.
-#                                                           All rights Reserved.
+# cpanel - ./t/lib/cPstrict.pm                     Copyright 2022 cPanel, L.L.C.
+#                                                           All rights reserved.
 # copyright@cpanel.net                                         http://cpanel.net
 # This code is subject to the cPanel license. Unauthorized copying is prohibited
 
