af5b18eec02b18521ec90ef9d32965cd2365b39b
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2023-04-18 12:46:45 -0400

    Isolate InfluxDB logic

---
+++ lib/Elevate/Components/InfluxDB.pm
@@ -0,0 +1,45 @@
+package Elevate::Components::InfluxDB;
+
+=encoding utf-8
+
+=head1 NAME
+
+Elevate::Components::InfluxDB
+
+Capture and reinstall InfluxDB packages.
+
+=cut
+
+use cPstrict;
+
+use Elevate::Constants ();
+
+use Cpanel::Pkgr  ();
+use Cwd           ();
+use Log::Log4perl qw(:easy);
+
+use parent qw{Elevate::Components::Base};
+
+sub pre_leapp ($self) {
+
+    cpev::remove_from_stage_file('reinstall.influxdb');
+
+    return unless Cpanel::Pkgr::is_installed('telegraf');
+
+    INFO("Not removing influxdb. Will re-install it after elevate.");
+    cpev::update_stage_file( { 'reinstall' => { 'influxdb' => 1 } } );
+
+    return;
+}
+
+sub post_leapp ($self) {
+
+    return unless cpev::read_stage_file('reinstall')->{'influxdb'};
+
+    INFO("Re-installing telegraf for influxdb");
+    $self->ssystem_and_die(qw{/usr/bin/yum -y reinstall telegraf});
+
+    return;
+}
+
+1;
