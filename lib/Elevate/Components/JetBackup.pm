eb42f84b1ac8894f5b4babac599bb396714c3e05
Nicolas Rochelemagne <nicolas.rochelemagne@webpros.com>
2023-04-18 12:41:32 -0400

    Isolate JetBackup to its component

---
+++ lib/Elevate/Components/JetBackup.pm
@@ -0,0 +1,63 @@
+package Elevate::Components::JetBackup;
+
+=encoding utf-8
+
+=head1 NAME
+
+Elevate::Components::JetBackup
+
+Capture and reinstall JetBackup.
+
+=cut
+
+use cPstrict;
+
+use Cpanel::Pkgr       ();
+use Elevate::Constants ();
+
+use Cwd           ();
+use Log::Log4perl qw(:easy);
+
+use parent qw{Elevate::Components::Base};
+
+sub pre_leapp ($self) {
+
+    cpev::remove_from_stage_file('reinstall.jetbackup');
+
+    return unless Cpanel::Pkgr::is_installed('jetbackup5-cpanel');
+
+    my $repos = cpev::yum_list();
+    my $jetbackup_tier =
+        $repos->{'jetapps-stable'} ? 'jetapps-stable'
+      : $repos->{'jetapps-edge'}   ? 'jetapps-edge'
+      : $repos->{'jetapps-beta'}   ? 'jetapps-beta'
+      :                              'jetapps-stable';    # Just give up and choose stable if you can't guess.
+    INFO("Jetbackup tier '$jetbackup_tier' detected. Not removing jetbackup. Will re-install it after elevate.");
+    my @reinstall = cpev::get_installed_rpms_in_repo(qw/jetapps jetapps-stable jetapps-beta jetapps-edge/);
+    unshift @reinstall, $jetbackup_tier;
+
+    my $data = {
+        tier     => $jetbackup_tier,
+        packages => \@reinstall,
+    };
+
+    cpev::update_stage_file( { 'reinstall' => { 'jetbackup' => $data } } );
+
+    return;
+}
+
+sub post_leapp ($self) {
+
+    my $data = cpev::read_stage_file('reinstall')->{'jetbackup'};
+    return unless ref $data && ref $data->{packages};
+
+    INFO("Re-installing jetbackup.");
+
+    my $tier     = $data->{tier};
+    my @packages = $data->{packages}->@*;
+    $self->ssystem( qw{/usr/bin/yum -y reinstall  --disablerepo=* --enablerepo=jetapps}, "--enablerepo=$tier", @packages );
+
+    return;
+}
+
+1;
