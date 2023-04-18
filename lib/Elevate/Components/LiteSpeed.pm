a3fd4e13fbe9dd51a792b8eaa287da5b0402f2bd
Nicolas Rochelemagne <nicolas.rochelemagne@webpros.com>
2023-04-18 12:50:01 -0400

    isolate LiteSpeed code

---
+++ lib/Elevate/Components/LiteSpeed.pm
@@ -0,0 +1,62 @@
+package Elevate::Components::LiteSpeed;
+
+=encoding utf-8
+
+=head1 NAME
+
+Elevate::Components::LiteSpeed
+
+Capture and reinstall LiteSpeed packages.
+
+=cut
+
+use cPstrict;
+
+use Elevate::Constants ();
+
+use Cwd           ();
+use Log::Log4perl qw(:easy);
+
+use parent qw{Elevate::Components::Base};
+
+sub pre_leapp ($self) {
+
+    cpev::remove_from_stage_file('reinstall.litespeed');
+
+    my $ls_cfg_dir = q[/usr/local/lsws/conf];
+    return unless -d $ls_cfg_dir;
+
+    INFO("LiteSpeed is installed");
+
+    # check if the license is valid before updating
+    $self->ssystem(qw{/usr/local/lsws/bin/lshttpd -V});
+    my $has_valid_license = $? == 0 ? 1 : 0;
+
+    my $data = {
+        has_valid_license => $has_valid_license,
+    };
+
+    cpev::update_stage_file( { 'reinstall' => { 'litespeed' => $data } } );
+
+    return;
+}
+
+sub post_leapp ($self) {
+
+    my $data = cpev::read_stage_file('reinstall')->{'litespeed'};
+    return unless ref $data;
+
+    INFO("Checking LiteSpeed");
+
+    # check the current license
+    if ( $data->{has_valid_license} ) {
+        $self->ssystem(qw{/usr/local/lsws/bin/lshttpd -V});
+        ERROR("LiteSpeed license is not valid. Check /usr/local/lsws/conf/serial.no") if $? != 0;
+    }
+
+    $self->ssystem(qw{/usr/bin/systemctl restart lsws});
+
+    return;
+}
+
+1;
