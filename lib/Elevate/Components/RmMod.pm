7de87e5bf6ea909f3d244498287a8b5a4cd27264
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2023-09-07 12:31:20 -0400

    Move some code from stage3 to components

---
+++ lib/Elevate/Components/RmMod.pm
@@ -0,0 +1,37 @@
+package Elevate::Components::RmMod;
+
+=encoding utf-8
+
+=head1 NAME
+
+Elevate::Components::RmMod
+
+Run rmmod
+
+=cut
+
+use cPstrict;
+
+use Elevate::Constants ();
+
+use Cwd           ();
+use File::Copy    ();
+use Log::Log4perl qw(:easy);
+
+use parent qw{Elevate::Components::Base};
+
+sub pre_leapp ($self) {
+
+    $self->run_once("_rmod_ln");
+
+    return;
+}
+
+sub _rmod_ln ($self) {
+
+    $self->ssystem( '/usr/sbin/rmmod', $_ ) foreach qw/floppy pata_acpi/;
+
+    return;
+}
+
+1;
