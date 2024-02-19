bc0e616df3de3ea326518ce0a5d900051244178d
Travis Holloway <t.holloway@cpanel.net>
2024-02-19 15:21:21 -0600

    Make CloudLinux 7 an experimental OS
    Case RE-208:  This change makes CloudLinux 7 an experimental OS and will
    give the user a warning before proceeding with the elevation if the
    server they are upgrading is considered experimental.
    
    Changelog:

--- lib/Elevate/OS/RHEL.pm
+++ lib/Elevate/OS/RHEL.pm
@@ -67,6 +67,7 @@ use constant available_upgrade_paths     => undef;
 use constant default_upgrade_to          => undef;
 use constant ea_alias                    => undef;
 use constant elevate_rpm_url             => undef;
+use constant is_experimental             => 0;
 use constant is_supported                => 1;
 use constant leapp_can_handle_epel       => 0;
 use constant leapp_can_handle_imunify    => 0;
