b12dc368f3e838ea0d25b9ccabf1a7f8ee4daabd
Tim Mullin <tim@cpanel.net>
2024-03-07 21:25:24 +0000

    Convert systems using Sectigo to use Let's Encrypt
    Case RE-54:
    
    When CentOS 7 is elevated to AlmaLinux 8, the "nobody"
    user will retain its uid/gid of 99 where as on AlmaLinux 8
    the expected uid/gid for "nobody" is 65534. This will
    cause issues with the Sectigo AutoSSL provider.
    If a system is setup to use Sectigo as the AutoSSL provider,
    emit a warning that the system will be switched to
    Lets Encrypt. Run the appropriate autofixer for the conversion
    during the upgrade
    
    Changelog: Change to Let's Encrypt if Sectigo is the AutoSSL provider.

---
+++ lib/Elevate/Components/AutoSSL.pm
@@ -0,0 +1,64 @@
+package Elevate::Components::AutoSSL;
+
+=encoding utf-8
+
+=head1 NAME
+
+Elevate::Components::AutoSSL
+
+Change AutoSSL provider from Sectigo to Lets Encrypt
+
+=cut
+
+use cPstrict;
+
+use Cpanel::SSL::Auto ();
+
+use parent qw{Elevate::Components::Base};
+
+sub pre_leapp ($self) {
+
+    if ( is_using_sectigo() ) {
+        $self->ssystem_and_die(qw{/usr/local/cpanel/scripts/autorepair set_autossl_to_lets_encrypt});
+    }
+
+    return;
+}
+
+sub post_leapp ($self) {
+
+    # Nothing to do
+    return;
+}
+
+=head2 is_using_sectigo()
+
+Determines whether AutoSSL is using Sectigo as a provider
+
+=head3 ARGUMENTS
+
+None
+
+=head3 RETURNS
+
+true/false if AutoSSL is/isn't using Sectigo
+
+=cut
+
+sub is_using_sectigo {
+
+    my @providers = Cpanel::SSL::Auto::get_all_provider_info();
+
+    foreach my $provider (@providers) {
+        next unless ( ref $provider eq 'HASH' && $provider->{enabled} );
+
+        if ( defined $provider->{display_name}
+            && $provider->{display_name} =~ /sectigo/i ) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+1;
