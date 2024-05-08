ac448e9cb22074dca9bcda7792fc1ff9397cf28d
David Lanning <davel@cpanel.net>
2024-05-08 14:16:03 -0500

    Add in missing apt conf.

--- .github/workflows/openstack/setup_terraform.sh
+++ .github/workflows/openstack/setup_terraform.sh
@@ -10,6 +10,10 @@ gpg --no-default-keyring \
     --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
     --fingerprint
 
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
 apt-get update
 
 apt-get install terraform
