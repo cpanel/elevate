6434d7099ebb5022a04c5c532b248097e4a69ad2
David Lanning <davel@cpanel.net>
2024-05-08 14:10:28 -0500

    WIP

--- .github/workflows/openstack/setup_terraform.sh
+++ .github/workflows/openstack/setup_terraform.sh
@@ -4,7 +4,7 @@ apt-get install -y gnupg software-properties-common || true
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \
     gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+    tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 
 gpg --no-default-keyring \
     --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
