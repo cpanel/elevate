e95c14501e2e85a0d9fc542c0c7a2b3293ff1a5a
David Lanning <davel@cpanel.net>
2024-05-09 13:49:38 -0500

    Adding in semi-working terraform.

---
+++ .github/workflows/openstack/main.tf
@@ -0,0 +1,77 @@
+# Define required providers
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.54.1"
+    }
+  }
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name                     = var.user
+  application_credential_id     = var.application_credential_id
+  application_credential_secret = var.application_credential_secret
+  auth_url                      = var.os_auth_url
+  region                        = var.os_auth_region
+}
+
+data "openstack_images_image_ids_v2" "images" {
+  name_regex = var.image_name
+  sort       = "updated_at"
+}
+
+data "template_cloudinit_config" "config" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    content_type = "text/cloud-config"
+    content      = file("cloud-config.yaml")
+  }
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+resource "random_string" "keyname" {
+  length  = 22
+  special = false
+}
+
+resource "openstack_compute_keypair_v2" "tf_remote_key" {
+  name   = "${random_string.keyname.result}-deletethis"
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+
+resource "openstack_compute_instance_v2" "elevatevm" {
+  name        = "elevatevm.aw.cpanel.net"
+  image_id    = sort(data.openstack_images_image_ids_v2.images.ids)[0]
+  flavor_name = var.flavor_name
+  key_pair    = openstack_compute_keypair_v2.tf_remote_key.name
+  user_data = "${data.template_cloudinit_config.config.rendered}"
+  network {
+    name = "hou-prod-external"
+  }
+
+  provisioner "remote-exec" {
+    inline = [<<EOF
+      echo "START_REMOTE_EXEC"
+      echo -e "\n\tStatus update from within TestVM - woop woop.\n\n"
+      echo "waiting on cloud-init..."
+      cloud-init status --wait > /dev/null || true
+    EOF
+    ]
+    connection {
+        type        = "ssh"
+        host        = self.access_ip_v4
+        user        = "root"
+        script_path = "/root/elevate_bootstrap"
+        private_key = tls_private_key.ssh.private_key_pem
+    }
+  }
+}
---
+++ .github/workflows/openstack/variables.tf
@@ -0,0 +1,49 @@
+variable "user" {
+  type = string
+  default = "resu"
+}
+
+variable "application_credential_id" {
+  type = string
+}
+
+variable "application_credential_secret" {
+  type = string
+}
+
+variable "os_password" {
+  type = string
+}
+
+variable "os_auth_region" {
+  type = string
+}
+
+variable "os_auth_url" {
+  type = string
+}
+
+variable "os_project_domain_name" {
+    type = string
+}
+
+variable "ssh_access_key" {
+  type = string
+  description = "SSH public key added to the VMs /root/.ssh/authorized_keys file to allow user access."
+  sensitive = true
+}
+
+variable "image_name" {
+  type    = string
+  default = "11.109.9999.* on CentOS 7"
+}
+
+variable "cpanel_release_version" {
+  type    = string
+  default = "110"
+}
+
+variable "flavor_name" {
+  type = string
+  default = "c2.d20.r2048"
+}
