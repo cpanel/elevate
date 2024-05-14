89a4ddf525cecb8ee55637e9590a8fdf936febd9
David Lanning <davel@cpanel.net>
2024-05-14 13:51:48 -0500

    add yes to elevate call.

--- .github/workflows/openstack/terraform/main.tf
+++ .github/workflows/openstack/terraform/main.tf
@@ -68,7 +68,12 @@ resource "openstack_compute_instance_v2" "elevatevm" {
       wget -O /scripts/elevate-cpanel https://raw.githubusercontent.com/cpanel/elevate/release/elevate-cpanel
       chmod -v 700 /scripts/elevate-cpanel
       /scripts/elevate-cpanel --check
-      /scripts/elevate-cpanel --start
+      /usr/local/cpanel/bin/whmapi1 start_background_mysql_upgrade version=10.6
+      sleep 120
+      perl -pi*.bak -e 's/^CPANEL=.*/CPANEL=11.110/g' /etc/cpupdate.conf
+      /scripts/upcp
+      sleep 30
+      yes | /scripts/elevate-cpanel --start
       /scripts/elevate-cpanel --status
     EOF
     ]
