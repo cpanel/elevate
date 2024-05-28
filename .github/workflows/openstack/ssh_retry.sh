a6a1326b73be41da50163a1b1b34cd0d9f016104
David Lanning <davel@cpanel.net>
2024-05-28 17:38:03 -0500

    Switch to netcat.

--- .github/workflows/openstack/ssh_retry.sh
+++ .github/workflows/openstack/ssh_retry.sh
@@ -4,22 +4,23 @@ set -x
 
 RETVAL=1;
 RETRIES=0;
-SSH_KEY=$1;
-HOST=$2;
+HOST=$1;
+PORT=$2
+PORT="${PORT:=22}";
 # Default 30 RETRIES usually one second apart
-RETRY_ARG=$3
-MAX_RETRIES="${RETRY_ARG:=30}";
+RETRY=$3
+RETRY="${RETRY:=30}";
 
 while [ $RETVAL -ne 0 ];
 do
-   # We want to exit immediately after we actually connect.
-   ssh -i $SSH_KEY root@$HOST exit 0;
+   # We want to exit immediately after we actually connect to SSH on default port.
+   nc -z ${HOST} ${PORT}
    RETVAL=$?;
-   [ $RETVAL -eq 0 ] && echo "## [INFO] SUCCESS: Connected to ${HOST} ##" && exit 0;
+   [ $RETVAL -eq 0 ] && echo "## [INFO] SUCCESS: Connected to SSH on ${HOST} ##" && exit 0;
    RETRIES=$((RETRIES+1));
-   [ $RETVAL -ne 0 ] && echo "Retrying: Attempt ${RETRIES} ...";
+   [ $RETVAL -ne 0 ] && echo "## [DEBUG]: Retrying SSH Connect: Attempt ${RETRIES} ...";
 
-   if [ ${RETRIES} -ge ${MAX_RETRIES} ];
+   if [ ${RETRIES} -ge ${RETRY} ];
    then
        echo "MAX_RETRIES has been reached.";
        exit 1;
