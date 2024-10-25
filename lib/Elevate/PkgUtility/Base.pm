946498e50ed3b43f2aced5535dc636a69eec1bcc
Travis Holloway <travis.holloway@webpros.com>
2024-10-25 11:51:12 -0500

    RE-252: Make the pkg utility logic generic

---
+++ lib/Elevate/PkgUtility/Base.pm
@@ -0,0 +1,26 @@
+package Elevate::PkgUtility::Base;
+
+=encoding utf-8
+
+=head1 NAME
+
+Elevate::PkgUtility::Base
+
+This is a base class used by Elevate::PkgUtility::*
+
+=cut
+
+use cPstrict;
+
+sub new ( $class, $opts = undef ) {
+    $opts //= {};
+
+    my $self = {%$opts};
+    bless $self, $class;
+
+    return $self;
+}
+
+sub name ($self) { die "name unimplemented" }
+
+1;
