3ec68dcbb8ee7123b9fbf385f3db8f883406b4e6
GitHub Action <github.bot@cpanel.net>
2025-02-19 23:36:03 +0000

    Regenerate Documentation Website

---
+++ docs/js/copy-code.js
@@ -0,0 +1,22 @@
+const copyButtonLabel = "Copy Code";
+
+// You can use a class selector instead if available.
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+  // only add button if browser supports Clipboard API
+  if (navigator.clipboard) {
+    let button = document.createElement("button");
+    button.innerText = copyButtonLabel;
+    button.addEventListener("click", copyCode);
+    block.appendChild(button);
+  }
+});
+
+async function copyCode(event) {
+  const button = event.srcElement;
+  const pre = button.parentElement;
+  let code = pre.querySelector("code");
+  let text = code.innerText;
+  await navigator.clipboard.writeText(text);
+}
